5f6ef67f02bdb212c578153ebebfd9ad94c5d96e
LEARNER17184 <202101497@daiict.ac.in>
2024-05-26 23:39:39 +0530

    bbokingmodel_routes_server.js_updated

---
+++ routes/bookingsRoute.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const bookingModel = require('../models/Booking')
+const roomModel = require('../models/room')
+const moment = require('moment')
+
+router.post('/bookroom',async (req,res) => {
+    const {room,userid,fromDate,toDate,totalAmount,totalDays} = req.body
+    
+    try {
+        const newBooking = new bookingModel({
+            room: room.name,
+            roomid: room._id,
+            userid,
+            fromDate: fromDate,
+            toDate: toDate,
+            totalAmount,
+            totalDays,
+            transactionId: '1234'    
+        })    
+        const booking = await newBooking.save()
+        const roomtemp = await roomModel.findOne({_id: room._id})
+        roomtemp.currentBookings.push({bookingid: booking._id, fromDate: fromDate,toDate: toDate,userid: userid,status: booking.status})
+        
+        await roomtemp.save()
+        
+        res.send('Room booked successfully')
+    } catch (error) {
+        return res.status(400).json({error})
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const dbConfig = require('./db')
 const userRouter = require('./routes/userRoute')
 const roomsRoute = require("./routes/roomsRoutes")
+const bookingsRoute = require("./routes/bookingsRoute")
 const cors = require('cors')
 
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.use('/api/users',userRouter)
-
+app.use('/api/bookings',bookingsRoute)
 app.use("/api/rooms", roomsRoute);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
